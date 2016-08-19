528b43fba6b6e37bfab88eaf3a864f916c6a7380
zacanger <zac@zacanger.com>
2016-08-19 15:44:09 -0600

    second

---
+++ README.md
@@ -0,0 +1,5 @@
+tiny cli for [who-that](https://www.npmjs.com/package/who-that)
+
+installation: `npm i -g who-that-cli`
+
+usage: `who-that username`
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const wt = require('who-that')
+#!/usr/bin/env node
 
-wt({
-  github: 'zacanger',
-  npm: 'zacanger'
-}).then(d => console.log(JSON.stringify(d)))
+const wt = require('who-that')
+const a1 = process.argv[2]
+const a2 = process.argv[2]
+
+if (!a1) {
+  return console.log('usage: who-that githubUsername npmUsername')
+}
+
+wt({github : a1, npm: a2}).then(d => console.log(JSON.stringify(d, null, 2)))
